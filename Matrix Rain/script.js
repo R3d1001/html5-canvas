912bcf8b04c603a650bced3f39ce38179777256c
R3d1001 <ahmarayaz1001@protonmail.com>
2022-12-27 18:40:35 +0500

    added fps, fixed a spelling issue

--- Matrix Rain/script.js
+++ Matrix Rain/script.js
@@ -10,6 +10,7 @@ class Symbol{
     constructor(x,y,fontSize,canvasHeight){
         //choose a charcter from this whenver jumping to a new position
         this.characters="アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        //this.characters="10"; alternate 01 binary characters list, uncomment and comment the above line to use
         this.x=x;
         this.y=y;
         this.fontSize=fontSize;
@@ -21,7 +22,7 @@ class Symbol{
         //randomise current character and draw in canvas at a specific position
         //math.floor removes decimals, and we get a random and multiply it by the size of characters array to get a random character from it
         this.text=this.characters.charAt(Math.floor(Math.random()*this.characters.length));
-        context.fillStyle="#0aff0a";//set shade
+
         //takes 3 arguments, text we want to draw, and x and y we multiply by fontsize so that they sit nicely below each other
         context.fillText(this.text,this.x*this.fontSize,this.y*this.fontSize);
         //math random makes it so that our columns all have different heights since they reset at different paces this way
@@ -33,7 +34,18 @@ class Symbol{
             this.y+=1;//increment y so that we move downwards along the canvas
         }
     }
+    invert(){
+        //TODO
+        //when called, changes characters from 01 to the specifiedand vice versa
+        if(!this.characters.length==2){
+            this.characters="01";
+        }
+        else{
+            this.characters="アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        }
+    }
 }
+
 
 class Effect{
     //the main wrapper for the entire rain effect, all of the symbols at once
@@ -57,19 +69,40 @@ class Effect{
     }
 }
 
-const effect=new Effect(canvas.width,canvas.height)
+const effect=new Effect(canvas.width,canvas.height);
+//for setting up fps
+let lastTime=0;
+const fps=30;//we want this many frames per second, lower reduces our performance usage
+const nextFrame=1000/fps; //1000 milli seconds, ie. 1 second divided by our fps
+let timer=0;//for accumilating delta time, when it reaches the threshold, animate next frame and reset itself to 0 and start counting again
+//delta time is the difference in milliseconds of the previous animation frame and the current animation frame
 
-function animate(){
-    //our animate function that runs and creates our animation
-    //for adding a fade effect, draw a semi transparent rectangle over it
-    ctx.fillStyle="rgba(0,0,0,0.05)";
-    ctx.fillRect(0,0,canvas.width,canvas.height);
+function animate(timeStamp){
+    //request animation frames has a timestamp variable which we pass
+    const deltaTime=timeStamp-lastTime;
+    lastTime=timeStamp;//so that we can use for the next loop
 
-    //monospace characters occupy the same ammount of horizontal space
-    ctx.font=effect.fontSize+"px monospace";
-    //call draw method of each symbol
-    effect.symbols.forEach(Symbol=>Symbol.draw(ctx));
+    //add an if condition so that we can enable our fps
+    if (timer>nextFrame) {
+
+        //our animate function that runs and creates our animation
+        //for adding a fade effect, draw a semi transparent rectangle over it
+        ctx.fillStyle="rgba(0,0,0,0.05)";
+        ctx.textAlign="center";//makes the text more even as there are japanese characters
+        ctx.fillRect(0,0,canvas.width,canvas.height);
+        ctx.fillStyle="#0aff0a";//set shade of the text
+        //monospace characters occupy the same ammount of horizontal space
+        ctx.font=effect.fontSize+"px monospace";
+        //call draw method of each symbol
+        effect.symbols.forEach(Symbol=>Symbol.draw(ctx));
+        timer=0;
+
+    }
+    else{
+        timer+=deltaTime;
+    }
+
     //for creating endless animation loop
     requestAnimationFrame(animate);
 }
-animate();
+animate(0);
